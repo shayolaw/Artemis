1beff8af295ee432ae420b9398aea17d3834f7f7
Shayo Law <shayolaw@gmail.com>
2019-07-22 11:06:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyparser = require('body-parser');
 const nodemailer = require('nodemailer');
 var secret='sk_test_94bb5a311bf89eb2b0ffc11b522de52184e07796';
 var paystack = require('paystack')(secret);
-const port = process.env.PORT || 3000;
 const xoauth2 = require('xoauth2');
 
 app.use('/Images',express.static(__dirname + "/Images"));
@@ -15,9 +14,13 @@ app.route('/')
 		res.sendFile(__dirname + '/first.html');
 		console.log('we are here');
 	});
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
- auth: {
+  const client_id ="1095060995264-5fa12n4gnp4v88cglm7tnc9i94i4ad9o.apps.googleusercontent.com";
+  const client_secret ="Lg1NKDDbxevkLwrZ_xzQtqCf";
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
         type: 'OAuth2',
         user: 'shayolaw@gmail.com',
         clientId: client_id,
