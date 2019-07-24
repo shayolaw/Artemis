778c63ba2b936dad0122ba6ad3ac61d5f03bb540
Shayo Law <shayolaw@gmail.com>
2019-07-24 14:42:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyparser = require('body-parser');
 const nodemailer = require('nodemailer');
 var secret='sk_test_94bb5a311bf89eb2b0ffc11b522de52184e07796';
 var paystack = require('paystack')(secret);
-const xoauth2 = require('xoauth2');
+const port = process.env.PORT || 3000;
 
 app.use('/Images',express.static(__dirname + "/Images"));
 app.use('/',express.static(__dirname + "/"));
@@ -14,21 +14,12 @@ app.route('/')
 		res.sendFile(__dirname + '/first.html');
 		console.log('we are here');
 	});
-  const client_id ="1095060995264-5fa12n4gnp4v88cglm7tnc9i94i4ad9o.apps.googleusercontent.com";
-  const client_secret ="Lg1NKDDbxevkLwrZ_xzQtqCf";
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        type: 'OAuth2',
-        user: 'shayolaw@gmail.com',
-        clientId: client_id,
-        clientSecret:client_secret,
-        refreshToken: "1/wHnOW0aiT3ejHIqPRxNHde_n1xMc2huReYhREHZb27c",
-        accessToken: "ya29.GltNBxllndbo3WOTW_8_v2XGdgcmHlPHozL_jyxeHMW3gAkEfMOqU4utp6qTG4GNVJ4VWYZ49eOrHCDH7jZGqtFQQBPcxoFskMS3GFE3urUZfu3SMlefFZQPV_s2",
-        expires: 3600
-    }
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'shayolaw@gmail.com',
+    pass: 'shayzzel'
+  }
 });
 app.route('/contact')
 	.post(function(req,res){
@@ -79,6 +70,6 @@ app.route('/hire')
       });
 	})
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-app.listen(3000,'127.0.0.1',function(){
+app.listen(port,function(){
 	console.log('We are in Mode');
 });
